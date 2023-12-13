9930b9b6bec27aa7e60d6b8cd83656e1f4a8a90d
dgrigoras2000 <150847661+dgrigoras2000@users.noreply.github.com>
2023-12-13 09:37:44 +0200

    Update example.c

--- example.c
+++ example.c
@@ -1,31 +1,16 @@
-A segmentation fault or core dump error often occurs due to memory-related issues, such as accessing memory that has already been freed, dereferencing a null or invalid pointer, or writing to read-only memory. In your code, there are a few points to check:
+// Function to print the list
+void printLinkedList() {
 
-Memory Allocation and Copying:
-Ensure that memory is allocated properly and that you're not overwriting memory boundaries. In your code, you are using memcpy to copy data into newNode->sectionName and newNode->status. Make sure that the allocated memory is large enough for the data being copied.
+    if (PendingList.head == NULL) {
+        WriteTrace(1,"List is empty.\n");
+        return;
+    }
 
-Example:
+    ConnectionsNode *current = PendingList.head;
 
-c
-Copy code
-newNode->sectionName = malloc(contentLength + 1);
-strncpy(newNode->sectionName, definedConnection, contentLength);
-newNode->sectionName[contentLength] = '\0';
+    while (current != NULL) {
+		WriteTrace(1, "Name: %s, Status: %s\n", current->sectionName, current->status);
 
-newNode->status = malloc(strlen(isConnected) + 1);
-strcpy(newNode->status, isConnected);
-Initialization of PendingList:
-Ensure that PendingList is properly initialized before calling this function. If it's a global variable or a static variable, make sure it's initialized at the beginning of your program.
-
-Example:
-
-c
-Copy code
-// Assuming PendingList is a global or static variable
-ConnectionsList PendingList = {NULL, NULL, 0};
-Freeing Memory:
-Ensure that you are freeing the allocated memory appropriately in other parts of your code. If there is a function to free the entire list, make sure it's called correctly.
-
-Function InitializeStatus:
-Verify that the InitializeStatus function is correctly implemented and does not cause any memory-related issues. If isConnected is a dynamically allocated string, ensure that it is freed appropriately.
-
-After making these adjustments, if the issue persists, consider using debugging tools like gdb to trace the exact location of the segmentation fault. Check the memory addresses involved and the line where the crash occurs. This will help you pinpoint the issue more precisely.
+        current = current->next;
+    }
+}
